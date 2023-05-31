a4d73a34906696c04feba0b169d0102a1c06aa6e
Timotej Tupý 104280 <79500031+timotej-stu@users.noreply.github.com>
2023-05-31 18:18:09 +0200

    added file_write

--- lokalizacia/src/lokalizacia_node.cpp
+++ lokalizacia/src/lokalizacia_node.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <numeric>
 #include <array>
+#include "fstream"
 
 class PoseAverager {
 private:
@@ -16,6 +17,9 @@ private:
     ros::Publisher publisher;
     ros::NodeHandle n;
 
+    size_t total_inputs = 0;
+    size_t correct_inputs = 0;
+
 public:
     PoseAverager() {
         subscriber = n.subscribe("/aruco_single/pose", 1000, &PoseAverager::poseCallback, this);
@@ -23,7 +27,27 @@ public:
         last_received = ros::Time::now();
     }
 
+    void writeToFile() {
+        std::ofstream outfile_x, outfile_z, outfile_percent;
+
+        outfile_x.open("average_x.txt", std::ios_base::app);
+        double average_x = computeAverage(positions[0]);
+        outfile_x << average_x << ", ";
+        outfile_x.close();
+
+        outfile_z.open("average_z.txt", std::ios_base::app);
+        double average_z = computeAverage(positions[2]);
+        outfile_z << average_z << ", ";
+        outfile_z.close();
+
+        outfile_percent.open("detections.txt", std::ios_base::app);
+        outfile_percent << "correct: " << correct_inputs << "     total: " << total_inputs << std::endl;
+        outfile_percent.close();
+    }
+
     void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+        total_inputs++;
+
         if (!last_received.isZero() && (ros::Time::now() - last_received).toSec() > 0.2) {
             ROS_WARN_STREAM("Preslo viac ako 200ms od poslednej uspesnej deteckcie. Array pozicii bol resetovany.");
             for(auto& position : positions) {
@@ -37,7 +61,7 @@ public:
             if (!positions[i].empty()) {
                 double average = computeAverage(positions[i]);
                 double stdev = computeStandardDeviation(positions[i], average);
-                if (std::abs(new_positions[i] - average) > 3.0 * std::max(stdev, 1.0)) {
+                if ((std::abs(new_positions[i] - average) >  10.0 * std::max(stdev, 0.8)) || new_positions[2] < 0.0) {
                     ROS_WARN_STREAM("Bola detekovana pozicia, ktora je signifikantne mimo priemeru. Tato pozicia bude ignorovana.");
                     return;
                 }
@@ -71,7 +95,9 @@ public:
             }
         }
 
+        correct_inputs++;
         publisher.publish(avg_pose);
+        writeToFile();
     }
 
     double computeStandardDeviation(const std::vector<double>& values, double mean) {
